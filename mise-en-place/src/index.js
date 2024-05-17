1144cbdafab7a328ed2624b9666136b268a72391
ViniCirello <vinicius.cirello@sptech.school>
2024-05-17 14:49:18 -0300

    fe_MiseEnPlace -> Criação componentes button

--- mise-en-place/src/index.js
+++ mise-en-place/src/index.js
@@ -4,9 +4,15 @@ import ReactDOM from 'react-dom/client';
 import Home from './pages/Home/home';
 import Header from './components/Header/header';
 import Sidebar from './components/Sidebar/sidebar';
+import Default from './components/Button/Default/default';
+import Cancelar from './components/Button/Cancelar/cancelar';
+import Disabled from './components/Button/Disabled/disabled';
+import Defaultv from './components/Button/Default-variant/defaultv';
+import Cancelarv from './components/Button/Cancelar-variant/cancelarv';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
-<Sidebar />
+<Cancelarv />
 </React.StrictMode>
 );
