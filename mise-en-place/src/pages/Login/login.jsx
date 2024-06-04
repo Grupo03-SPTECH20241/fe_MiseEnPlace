46c0b10d2db18d2edeefe7ec9d1362136294e9c5
ViniCirello <vinicius.cirello@sptech.school>
2024-06-04 20:55:12 -0300

    fe_MiseEnPlace -> Integração Tela Login com o back

--- mise-en-place/src/pages/Login/login.jsx
+++ mise-en-place/src/pages/Login/login.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import LoginStyles from './login.module.css';
 import ConfeiteiraImage from '../../utils/img/ConfeiteiraImage.svg';
+import api from '../../api';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        api.post('/usuarios/login', { "email": email, "senha": password }).then((response) => {
+            console.log(response, "deu certo")
+        }).catch((error) => {
+            console.log(error, "error")
+        })
+    };
 
     return (
         <div className={LoginStyles['App']}>
@@ -14,18 +27,18 @@ function Login() {
                     </div>
                     <img src={ConfeiteiraImage} alt="Confeiteira" className={LoginStyles['confeiteira']} />
                 </div>
-                <div className={LoginStyles['form-content']}>
+                <form onSubmit={handleLogin} className={LoginStyles['form-content']}>
                     <span>Mise en Place</span>
                     <div className={LoginStyles['form-group']}>
                         <label>E-mail:</label>
-                        <input type="email" name="email" placeholder="Inserir e-mail" />
+                        <input type="email" name="email" placeholder="Inserir e-mail" onChange={e => setEmail(e.target.value)} />
                     </div>
                     <div className={LoginStyles['form-group']}>
                         <label>Senha:</label>
-                        <input type="password" name="companyName" placeholder="Insira sua senha" />
+                        <input type="password" name="password" placeholder="Insira sua senha" onChange={e => setPassword(e.target.value)} />
                     </div>
                     <button type="submit">Entrar</button>
-                </div>
+                </form>
             </div>
         </div>
     );
