416d82ffebbf7e5f106be1576d551d888470c2da
KevynRochaAl <kevyn.alves@sptech.school>
2024-06-17 17:53:13 -0300

    Correção do botão da meta

--- mise-en-place/src/components/Button/Cancelar-variant/cancelarv.jsx
+++ mise-en-place/src/components/Button/Cancelar-variant/cancelarv.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './cancelarv.module.css';
 import PlusImage from '../../../utils/img/plus.png'
+import SettingsImage from '../../../utils/img/settings_icon.png'
 
 const Button = ({label}) => {
   return (
     <button className={styles['cancel-variant-button']}>
-      <img src={PlusImage} alt="Button icon" />
+      {/* <img src={PlusImage} alt="Button icon" /> */}
+      <img src={SettingsImage} alt="Button icon" />
       <span>{label ? label : 'Label'}</span>
-      <img src={PlusImage} alt="Button icon" />
+      {/* <img src={PlusImage} alt="Button icon" /> */}
     </button>
   );
 };
