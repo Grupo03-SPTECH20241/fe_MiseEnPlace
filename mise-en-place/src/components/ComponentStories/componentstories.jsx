350058c51911eda6af6f0fced15bbd9e6838dcfd
KevynRochaAl <kevyn.alves@sptech.school>
2024-08-07 19:44:03 -0300

    Adição de propses para o botão de Cancelar

--- mise-en-place/src/components/ComponentStories/componentstories.jsx
+++ mise-en-place/src/components/ComponentStories/componentstories.jsx
@@ -1,49 +1,90 @@
 import React from 'react';
-import Button from '../Button/Cancelar-variant/cancelarv'
+import ButtonOutlinedNegative from '../Button/Cancelar-variant/cancelarv'
+import ButtonFilledNegative from '../Button/Cancelar/cancelar';
 import styles from './componentstories.module.css';
 
 const Stories = () => {
   return (
     <>
     <div className={styles['component-stories-container']}>
-        <h2>Botão sem ícone</h2>
+        <h2>ButtonOutlinedNegative</h2>
         <div className={styles['row']}>
             <div className={styles['row-item']}>
                 <h5>Botão sem ícone</h5>
-                <Button
+                <ButtonOutlinedNegative
                     showIcon='false'
-                ></Button>
+                ></ButtonOutlinedNegative>
             </div>
 
             <div className={styles['row-item']}>
                 <h5>Botão com ícone</h5>
-                <Button
+                <ButtonOutlinedNegative
                     showIcon='true'
-                ></Button>
+                ></ButtonOutlinedNegative>
             </div>
 
             <div className={styles['row-item']}>
                 <h5>Botão com ícone esquerdo</h5>
-                <Button
+                <ButtonOutlinedNegative
                     showIcon='true'
                     iconPosition='left'
-                ></Button>
+                ></ButtonOutlinedNegative>
             </div>
 
             <div className={styles['row-item']}>
                 <h5>Botão com ícone direito</h5>
-                <Button
+                <ButtonOutlinedNegative
                     showIcon='true'
                     iconPosition='right'
-                ></Button>
+                ></ButtonOutlinedNegative>
             </div>
 
             <div className={styles['row-item']}>
                 <h5>Botão com ícone customizado</h5>
-                <Button
+                <ButtonOutlinedNegative
                     showIcon='true'
                     icon='settings'
-                ></Button>
+                ></ButtonOutlinedNegative>
+            </div>
+        </div>
+        <h2>ButtonFilledNegative</h2>
+        <div className={styles['row']}>
+            <div className={styles['row-item']}>
+                <h5>Botão sem ícone</h5>
+                <ButtonFilledNegative
+                    showIcon='false'
+                ></ButtonFilledNegative>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone</h5>
+                <ButtonFilledNegative
+                    showIcon='true'
+                ></ButtonFilledNegative>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone esquerdo</h5>
+                <ButtonFilledNegative
+                    showIcon='true'
+                    iconPosition='left'
+                ></ButtonFilledNegative>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone direito</h5>
+                <ButtonFilledNegative
+                    showIcon='true'
+                    iconPosition='right'
+                ></ButtonFilledNegative>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone customizado</h5>
+                <ButtonFilledNegative
+                    showIcon='true'
+                    icon='settings'
+                ></ButtonFilledNegative>
             </div>
         </div>
     </div>
