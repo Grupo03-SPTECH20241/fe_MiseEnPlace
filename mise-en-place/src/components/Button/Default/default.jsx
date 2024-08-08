b9d8d9536a89a397e00b6dc99d433f7ea9b28812
KevynRochaAl <kevyn.alves@sptech.school>
2024-08-08 18:41:05 -0300

    Adição de propses para o restante dos botões

--- mise-en-place/src/components/Button/Default/default.jsx
+++ mise-en-place/src/components/Button/Default/default.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';  
 import styles from './default.module.css';
 import PlusImage from '../../../utils/img/plus.png'
+import SettingsImage from '../../../utils/img/settings_icon.png';
 
-const Button = () => {
+const ButtonFilledDefault = ({ label = 'Label', showIcon = 'true', icon = 'plus', iconPosition = 'both', fontSize = 'medium' }) => {
+  // Renderiza ícones com base na posição  
+  const renderIcon = (iconId) => {  
+    if (showIcon !== 'true') return null;  
+
+    if (showIcon === 'true' && iconId === 0 && iconPosition === 'left' || iconPosition === 'both') {  
+      return(<img key="left-icon" src={icon === 'settings'? SettingsImage : PlusImage}/>);  
+    }  
+    if (showIcon === 'true' && iconId === 1 && iconPosition === 'right' || iconPosition === 'both') {  
+      return(<img key="right-icon" src={icon === 'settings'? SettingsImage : PlusImage}/>);  
+    }  
+    return null;  
+  };  
+
   return (
-    <button className={styles['default-button']}>
-      <img src={PlusImage} alt="Button icon" />
-      Button
-      <img src={PlusImage} alt="Button icon" />
-
-    </button>
+    <button className={styles['default-button']}>  
+      {renderIcon(0)}  
+      <span className={styles[`font-size-${fontSize}`]}>{label}</span>  
+      {renderIcon(1)}  
+    </button>  
   );
 };
 
-export default Button;
+// Definindo as PropTypes para o componente  
+ButtonFilledDefault.propTypes = {  
+  label: PropTypes.string,  
+  showIcon: PropTypes.bool,  
+  icon: PropTypes.oneOf(['plus','settings']),  
+  iconPosition: PropTypes.oneOf(['left', 'right', 'both']),  
+  fontSize: PropTypes.oneOf(['small', 'medium', 'large'])
+};  
+
+export default ButtonFilledDefault;
--- mise-en-place/src/components/ComponentStories/componentstories.jsx
+++ mise-en-place/src/components/ComponentStories/componentstories.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from './componentstories.module.css';
+
 import ButtonOutlinedNegative from '../Button/Cancelar-variant/cancelarv'
 import ButtonFilledNegative from '../Button/Cancelar/cancelar';
-import styles from './componentstories.module.css';
+import ButtonFilledDefault from '../Button/Default/default';
+import ButtonFilledDefaultVariant from '../Button/Default-variant/defaultv';
+import ButtonFilledDisabled from '../Button/Disabled/disabled';
 
 const Stories = () => {
   return (
@@ -102,6 +106,150 @@ const Stories = () => {
                     showIcon='true'
                     fontSize='small'
                 ></ButtonFilledNegative>
+            </div>
+        </div>
+        <h2>ButtonFilledDefault</h2>
+        <div className={styles['row']}>
+            <div className={styles['row-item']}>
+                <h5>Botão sem ícone & fonte grande</h5>
+                <ButtonFilledDefault
+                    showIcon='false'
+                    fontSize='large'
+                ></ButtonFilledDefault>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone</h5>
+                <ButtonFilledDefault
+                    showIcon='true'
+                ></ButtonFilledDefault>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone esquerdo</h5>
+                <ButtonFilledDefault
+                    showIcon='true'
+                    iconPosition='left'
+                ></ButtonFilledDefault>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone direito</h5>
+                <ButtonFilledDefault
+                    showIcon='true'
+                    iconPosition='right'
+                ></ButtonFilledDefault>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone customizado</h5>
+                <ButtonFilledDefault
+                    showIcon='true'
+                    icon='settings'
+                ></ButtonFilledDefault>
+            </div>
+            <div className={styles['row-item']}>
+                <h5>Botão com fonte pequena</h5>
+                <ButtonFilledDefault
+                    showIcon='true'
+                    fontSize='small'
+                ></ButtonFilledDefault>
+            </div>
+        </div>
+        <h2>ButtonFilledDefaultVariant</h2>
+        <div className={styles['row']}>
+            <div className={styles['row-item']}>
+                <h5>Botão sem ícone & fonte grande</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='false'
+                    fontSize='large'
+                ></ButtonFilledDefaultVariant>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='true'
+                ></ButtonFilledDefaultVariant>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone esquerdo</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='true'
+                    iconPosition='left'
+                ></ButtonFilledDefaultVariant>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone direito</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='true'
+                    iconPosition='right'
+                ></ButtonFilledDefaultVariant>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone customizado</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='true'
+                    icon='settings'
+                ></ButtonFilledDefaultVariant>
+            </div>
+            <div className={styles['row-item']}>
+                <h5>Botão com fonte pequena</h5>
+                <ButtonFilledDefaultVariant
+                    showIcon='true'
+                    fontSize='small'
+                ></ButtonFilledDefaultVariant>
+            </div>
+        </div>
+        <h2>ButtonFilledDisabled</h2>
+        <div className={styles['row']}>
+            <div className={styles['row-item']}>
+                <h5>Botão sem ícone & fonte grande</h5>
+                <ButtonFilledDisabled
+                    showIcon='false'
+                    fontSize='large'
+                ></ButtonFilledDisabled>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone</h5>
+                <ButtonFilledDisabled
+                    showIcon='true'
+                ></ButtonFilledDisabled>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone esquerdo</h5>
+                <ButtonFilledDisabled
+                    showIcon='true'
+                    iconPosition='left'
+                ></ButtonFilledDisabled>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone direito</h5>
+                <ButtonFilledDisabled
+                    showIcon='true'
+                    iconPosition='right'
+                ></ButtonFilledDisabled>
+            </div>
+
+            <div className={styles['row-item']}>
+                <h5>Botão com ícone customizado</h5>
+                <ButtonFilledDisabled
+                    showIcon='true'
+                    icon='settings'
+                ></ButtonFilledDisabled>
+            </div>
+            <div className={styles['row-item']}>
+                <h5>Botão com fonte pequena</h5>
+                <ButtonFilledDisabled
+                    showIcon='true'
+                    fontSize='small'
+                ></ButtonFilledDisabled>
             </div>
         </div>
     </div>
