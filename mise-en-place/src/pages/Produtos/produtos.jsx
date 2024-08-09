14b2e6d8aca30857083e99844f92c32ad7a9cdc2
HenriqueBechis <henrique.coelho@sptech.school>
2024-08-08 21:10:12 -0300

    featura/filtrotipo

--- mise-en-place/src/pages/Produtos/produtos.jsx
+++ mise-en-place/src/pages/Produtos/produtos.jsx
@@ -4,29 +4,45 @@ import Breadcrumb from '../../components/Texts/Breadcrumbs/breadcrumbs';
 import styles from './produto.module.css';
 import Button from '../../components/Button/Product/product';
 import InputSearch from '../../components/Input/SearchProduct/searchProduct';
-import InputSelect from '../../components/Input/SelectProduct/selectProduct';
+import InputSelect from '../../components/Input/SelectProduct/selectProduct'
+import Filter from '../../components/Filter/filter';
 import CardProduct from '../../components/CardProduct/CardProduct';
 import api from '../../api';
 
-<link    href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600&display=swap" rel="stylesheet"></link> 
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600&display=swap" rel="stylesheet"></link> 
+
+
+
 
 const Produtos = () => {
     const [cardsData, setCardsData] = useState();
+    const [value, setValue] = useState();
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await api.get('/produtos');
-                const { data } = response;
-                setCardsData(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+        fetchDataDrop();
         fetchData();
-        const tipoProdutoInterval = setInterval(fetchData, 5000);
-        return () => clearInterval(tipoProdutoInterval);
+        // const tipoProdutoInterval = setInterval(fetchData, 5000);
+        // return () => clearInterval(tipoProdutoInterval);
     }, []);
 
+    const fetchDataDrop = () => {
+        api.get('/tipo-produtos').then((response) => {
+            setValue(response.data.map((item) => item.nome));
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    const fetchData = () => {
+        api.get('/produtos').then((response) => {
+            setCardsData(response.data);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    const handleChange = (value) => {
+        console.log(value);
+    }
     
     return (
         <div className={styles["mainContainer"]}>
@@ -36,14 +52,18 @@ const Produtos = () => {
                     <Breadcrumb></Breadcrumb>
                 </div>
                 <div className={styles["produtoTittleCard"]}>
-                    <h2>Produtos</h2>
-                    <p> Lista de todos os produtos cadastrados no sistema</p>
+                    <h2>Produtos, Dropdown</h2>
+                    <p> Lista de todos os produtos, Dropdown cadastrados no sistema</p>
                 </div>
                 <div className={styles["innerContainerSearch"]}>
                     <Button></Button>
                     <div className={styles["containerInputs"]}>
                         <InputSearch></InputSearch>    
-                        <InputSelect></InputSelect>
+
+                        <Filter options= {value ? value : ['']}
+                            onChange={handleChange}
+                        ></Filter> 
+                        
 
                     </div>
                 </div>
@@ -51,7 +71,8 @@ const Produtos = () => {
                 
                 <div className= {styles["content-musicas"]}>
 
-                {cardsData && cardsData.map((data, index) => (
+                {
+                cardsData && cardsData.map((data, index) => (
                     <CardProduct
                     key={index}
                     descricao={data.descricao}
