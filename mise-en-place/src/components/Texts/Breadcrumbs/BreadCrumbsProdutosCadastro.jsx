2bef9eae62785d3f9e67ec1c5c6dba1f6d9f5619
leandroTokudome <leandro.tokudome@sptech.school>
2024-08-24 22:34:38 -0300

    Tela de cadastro de produto

---
+++ mise-en-place/src/components/Texts/Breadcrumbs/BreadCrumbsProdutosCadastro.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Home from '../../../utils/img/home.png';
+import style from './breadcrumbs.module.css';
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+export default function ActiveLastBreadcrumb() {
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="text.primary" href="/">
+          <img className={style['breadcrumbs-arrow']} src={Home} alt="Seta"/>
+        </Link>
+        <Link underline="hover" color="text.primary" href="/Login">Home</Link>
+        <Link underline="hover" color="text.primary" href="/produtos">Produtos</Link>
+      </Breadcrumbs>
+    </div>
+  );
+}
---
+++ mise-en-place/src/pages/ProdutoCadastro/produtoCadastro.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import Sidebar from '../../components/Sidebar/sidebar';
+import BreadcrumbProdutoCadastro from '../../components/Texts/Breadcrumbs/BreadCrumbsProdutosCadastro';
+import styles from './produtoCadastro.module.css';
+import CameraIcon from "../../utils/img/icons/camera.png"
+import Input from "../../components/Input/Text/text"
+import InputMaskCustom from "../../components/Input/InputMask/inputMaskCustom"
+import Button from "../../components/Button/Default/default"
+
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600&display=swap" rel="stylesheet"></link>
+
+const ProdutoCadastro = () => {
+    return (
+        <div className={styles["mainContainer"]}>
+            <Sidebar />
+            <div className={styles["innerContainer"]}>
+                <div className={styles["produtoBreadcrumbsContainer"]}>
+                    <BreadcrumbProdutoCadastro />
+                </div>
+                <div className={styles["produtoCadastroTittleCard"]}>
+                    <h2>Cadastrar produto</h2>
+                    <p>Cadastre um novo produtinho para os seus clientes.</p>
+                </div>
+
+                <div className={styles["produtoCadastroMainContainer"]}>
+                    <div className={styles["imageContainer"]}>
+                        <div className={styles["produtoCadastroImage"]}>
+                            <img src={CameraIcon} />
+                        </div>
+                    </div>
+                    <div className={styles["inputsContainer"]}>
+                        <div className={styles["inputsContainerLine1"]}>
+                            <Input
+                                label='Nome do produto:'
+                                placeholder='Insira o nome do produto'
+                                fieldWidth="65%"
+                                width='100%'>
+                            </Input>
+                            <InputMaskCustom
+                                label='Preço:'
+                                placeholder='R$'
+                                id='precoInput'
+                                width='100%'
+                                fieldWidth="30%"
+                                mask="R$ 999999999"
+                                maskChar={null}>
+                            </InputMaskCustom>
+                        </div>
+                        <div className={styles["inputsContainerLine2"]}>
+                            <Input
+                                label='Massa:'
+                                placeholder='Insira o tipo de massa'
+                                fieldWidth="48%"
+                                width='100%'>
+                            </Input>
+                            <Input
+                                label='Cobertura:'
+                                placeholder='Insira o tipo de cobertura'
+                                fieldWidth="48%"
+                                width='100%'>
+                            </Input>
+                        </div>
+                        <div className={styles["inputsContainerLine3"]}>
+                            <Input
+                                label='Recheio:'
+                                placeholder='Selecione um recheio'
+                                fieldWidth="48%"
+                                width='100%'>
+                            </Input>
+                            <InputMaskCustom
+                                label='Preço do recheio:'
+                                placeholder='R$'
+                                id='precoCoberturaInput'
+                                width='100%'
+                                fieldWidth="48%"
+                                mask="R$ 999999999"
+                                maskChar={null}>
+                            </InputMaskCustom>
+                        </div>
+                        <div className={styles["inputsContainerLine4"]}>
+                            <Input
+                                label='Descrição:'
+                                placeholder='Insira a descrição do novo produto'
+                                fieldWidth="100%"
+                                width='100%'>
+                            </Input>
+                        </div>
+                        <div className={styles["inputsContainerLine5"]}>
+                            <Button
+                                label='Cadstrar produto'
+                                icon='plus'
+                                iconPosition='left'
+                                width='25%'>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProdutoCadastro;
