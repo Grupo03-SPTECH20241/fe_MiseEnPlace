a80bf5a9e1c5745773286f52d8bb2b35a01b55a1
gustavodessunte <gustavo.bernardes@sptech.school>
2024-11-20 19:15:56 -0300

    api.js -> Adicionando exeção de token para cadastro de usuarios

--- mise-en-place/src/api.js
+++ mise-en-place/src/api.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+  baseURL: "http://localhost:8080"
 });
 
 api.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('token');
-  
-      if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-      }
-      
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token && config.url !== '/usuario') {
+      console.log(config.url);
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
