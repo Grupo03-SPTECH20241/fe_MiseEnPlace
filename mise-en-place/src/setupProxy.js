444ab99c09b831ed8a99e0b50fe47db2e84eb24c
KevynRochaAl <kevyn.alves@sptech.school>
2024-12-02 18:43:05 -0300

    Adição de comentários

--- mise-en-place/src/setupProxy.js
+++ mise-en-place/src/setupProxy.js
@@ -4,7 +4,9 @@ module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: 'http://10.0.0.156:8080',
+            // caso queira testar local, comente a NUVEM, caso queira testar na nuvem, comente a LOCAL.
+            //target: 'http://127.0.0.1:8080', // LOCAL
+            target: 'http://10.0.0.156:8080', // NUVEM
             changeOrigin: true,
         })
     );
