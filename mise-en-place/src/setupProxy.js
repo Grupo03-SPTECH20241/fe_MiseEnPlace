0cd1a4278c4e0bed09a727f1990f109743c9b5e8
KevynRochaAl <kevyn.alves@sptech.school>
2024-12-03 20:24:59 -0300

    remoção da acessibilidade

--- mise-en-place/src/setupProxy.js
+++ mise-en-place/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
         '/java-api',
         createProxyMiddleware({
             // caso queira testar local, comente a NUVEM, caso queira testar na nuvem, comente a LOCAL.
-            //target: 'http://127.0.0.1:8080', // LOCAL
+            // target: 'http://127.0.0.1:8080', // LOCAL
             target: 'http://10.0.0.156:8080', // NUVEM
             changeOrigin: true,
         })
